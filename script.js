e0fbf87c15b80cfdf4be06d2ce0aff3783c69c08
James Blake <142352216+blakejam371@users.noreply.github.com>
2024-04-21 16:46:00 +0100

    Check password matches
    - Show message on screen if password matches or
    doesn't match.
    - Change message red if not matching
    - Grey-out the 'submit' button if password doesn't
    match

---
+++ script.js
@@ -0,0 +1,25 @@
+const pass = document.querySelector('#pass');
+const passConf = document.querySelector('#passconf');
+const passRules = document.querySelector('#pass-rules');
+const passMatch = document.querySelector('#pass-match');
+const btn = document.querySelector('#submit');
+
+passConf.addEventListener("input", () => {
+  if (passConf.value !== pass.value) {
+    btn.setAttribute('disabled', '');
+    passMatch.style.color = 'red';
+    passMatch.style.fontWeight = '600';
+    return passMatch.textContent = 'Passwords not matching'
+  } 
+  else {
+    btn.removeAttribute('disabled');
+    return passMatch.textContent = 'Passwords match';
+  };
+});
+
+
+// `Password must have:
+//   - One uppercase character
+//   - One lowercase character
+//   - One number or special character
+//   - Minimum eight characters`;
